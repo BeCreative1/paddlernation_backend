e745a60daffdfca7df7d428e99af9c41018bcec3
Enbi81 <wotng2002@gmail.com>
2023-03-25 15:28:38 +0100

    creating unit test example

---
+++ UnitTests/ApplicationTests/TestExample.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xUnit.Utils;
+
+namespace xUnit.ApplicationTests;
+
+
+[TestClass]
+public class TestExample : DbTestBaseClass
+{
+    [TestMethod]
+    public void TestBasicInitialize()
+    {
+        int shouldBeZero = PaddleBoardDb.PaddleBoardTypes.Count();
+        Assert.AreEqual(0, shouldBeZero);
+
+        
+        var newPaddleBoard = new PaddleboardType
+        {
+            Name = "TestPaddleBoard",
+            MaxCapacity = 10,
+            MinCapacity = 1,
+            Quantity = 3,
+        };
+        
+        PaddleBoardDb.PaddleBoardTypes.Add(newPaddleBoard);
+        PaddleBoardDb.SaveChanges();
+
+        
+        int shouldBeOne = PaddleBoardDb.PaddleBoardTypes.Count();
+        Assert.AreEqual(1, shouldBeOne);
+    }
+    
+    
+}
---
+++ UnitTests/Utils/DbTestBaseClass.cs
@@ -0,0 +1,28 @@
+﻿using EfcDataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace xUnit.Utils;
+
+public class DbTestBaseClass
+{
+    public PaddleBoardDbContext PaddleBoardDb { get; private set; }
+
+    [TestInitialize]
+    public virtual void TestInit()
+    {
+        // configure an in memory database
+        PaddleBoardDb = new PaddleBoardDbContextInMemory();
+        // this ensures that the database is created in the memory and it's ready to use 
+        PaddleBoardDb.Database.EnsureCreated();
+    }
+
+
+    private class PaddleBoardDbContextInMemory : PaddleBoardDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString());
+        }
+    }
+}
