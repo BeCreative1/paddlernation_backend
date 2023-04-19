d24e13aac6db247bcc5102fc76bcc7b6878c249e
Simon Mayer <sma@jansson.dk>
2023-04-19 20:03:51 +0200

    Implemented CustomersController.cs with Post and Get endpoints

--- EfcDataAccess/DAOs/CustomerEfcDao.cs
+++ EfcDataAccess/DAOs/CustomerEfcDao.cs
@@ -6,15 +6,32 @@ namespace EfcDataAccess.DAOs;
 
 public class CustomerEfcDao : ICustomerDao
 {
-    private readonly PaddlerNationContext context;
+    private readonly PaddlerNationContext _context;
 
     public CustomerEfcDao(PaddlerNationContext context)
     {
-        this.context = context;
+        _context = context;
     }
+
+    public async Task<int> CreateAsync(Customer customer)
+    {
+        var createdCustomer = await _context.Customers.AddAsync(customer);
+        return createdCustomer.Entity.Id;
+    }
+
     public async Task<Customer?> GetByIdAsync(int id)
     {
-        Customer? customer = await context.Customers.FindAsync(id);
+        Customer? customer = await _context.Customers.FindAsync(id);
         return customer;    
+    }
+
+    public async  Task<int> CustomerExists(Customer customer)
+    {
+        var existingCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.FullName.Equals(customer.FullName) && c.Email.Equals(customer.Email) && c.Phone.Equals(customer.Phone));
+
+        if (existingCustomer is not null)
+            return existingCustomer.Id;
+
+        return -1;
     }
 }
