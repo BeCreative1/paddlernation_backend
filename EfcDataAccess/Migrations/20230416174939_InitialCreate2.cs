0e896450e920f2c3eaa32254c357dff1a0066ea1
xoxkoo <anton.durcak22@gmail.com>
2023-04-21 17:08:28 +0200

    reservation

---
+++ EfcDataAccess/Migrations/20230416174939_InitialCreate2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfcDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PaddleBoardReservations",
+                table: "PaddleBoardReservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PaddleBoardReservations_PadleBoardID",
+                table: "PaddleBoardReservations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PaddleBoardReservations",
+                table: "PaddleBoardReservations",
+                columns: new[] { "PadleBoardID", "ReservationID" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaddleBoardReservations_ReservationID",
+                table: "PaddleBoardReservations",
+                column: "ReservationID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PaddleBoardReservations",
+                table: "PaddleBoardReservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PaddleBoardReservations_ReservationID",
+                table: "PaddleBoardReservations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PaddleBoardReservations",
+                table: "PaddleBoardReservations",
+                columns: new[] { "ReservationID", "PadleBoardID" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaddleBoardReservations_PadleBoardID",
+                table: "PaddleBoardReservations",
+                column: "PadleBoardID");
+        }
+    }
+}
